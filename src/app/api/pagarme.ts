799a2e429877dc5a72c4bd38c4c4a15af06f618f
carlos Siqueira <carlooos.siqueira@gmail.com>
2025-01-20 12:12:48 -0300

    fix: remove auth pagarme from code

--- src/app/api/pagarme.ts
+++ src/app/api/pagarme.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const pagarme = axios.create({
   baseURL: 'https://api.pagar.me/core/v5',
   headers: {
-    Authorization: 'Basic c2tfNjUyODNlOTllODRlNDQ1OThkMTMwN2VmODU5ZTgxNTE6',
+    Authorization: `Basic ${process.env.AUTH_PAGARME}`,
     'Content-Type': 'application/json'
   }
 })
